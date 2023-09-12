0bb3bf417838becab54fb0e8350dcf131446f439
mkurtoglu <muhammet.kurtoglu@bisoft.com.tr>
2023-09-12 11:53:00 +0300

    Update postgresql_index_advice.sh

--- postgresql_index_advice.sh
+++ postgresql_index_advice.sh
@@ -3,7 +3,7 @@ psql -p$1 -d $2 -c "explain $3"  -t -o explain.out
 cat explain.out | grep   -A 3  "Seq Scan" | grep  -B 2  Filter |while read line
 do
     if [ ! -z "${prev}" ];then
-        line1="${prev}"
+        line1=`echo "${prev}" | grep -E 'Scan.*on\s*([a-zA-Z]+\w*)' `
         line2="${line}"
         tablo=`echo  "${line1}  ${line2}" | grep -E 'Scan.*on\s*([a-zA-Z]+\w*)'  | grep 'Seq Scan' | sed 's/.*Seq Scan on//'  | sed 's/ (cost.*//' | awk '{print $1;}'`
         kolonlar=`echo  "${line1}  ${line2}" | grep -E 'Scan.*on\s*([a-zA-Z]+\w*)'  | grep 'Filter' | sed 's/.*Filter: //'  `
