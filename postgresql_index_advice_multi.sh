aaf2cc95e7194453f44f9de6f56a957d4f9553af
Ozgur Yuksel <ozgur.yuksel@bisoft.com.tr>
2024-01-10 17:59:57 +0300

    print mode single lining

--- postgresql_index_advice_multi.sh
+++ postgresql_index_advice_multi.sh
@@ -4,7 +4,20 @@
 # and then to feed them to postgresql_index_advice.sh
 #
 
+#
 # TODO: Show usage by default or with --help
+#
 
-
+#
 # TODO: Print different SQL statements (even if they are multi line)
+#
+# CURRENTLY: This expects a blank line at the end of the file
+
+INFILE="testinput.sql"
+LINE=''
+
+while read -r LINE
+do
+    echo -n "$LINE "
+    printf '%s\n' "$LINE" | grep -q ";[ ]*$" && echo 
+done < "$INFILE"
